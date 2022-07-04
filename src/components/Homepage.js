ff3282c0ca1f42b4cf79095664376da152654bab
Clara CHAOUAT <chaouatclara337@hotmail.com>
2022-07-04 20:32:51 +0200

    Connect country code to react query

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -2,46 +2,54 @@ import "./homepage.css";
 import { useQuery } from "react-query";
 import React, { useState } from "react";
 
-export default function Gettinginfo() {
-  const [country, getCountry] = useState(null);
-  const [search, setSearch] = useState(false);
-  const apiKey = `4df426ce1423e6a019e20c71208dd422ffd9d376`;
+const apiKey = `4df426ce1423e6a019e20c71208dd422ffd9d376`;
+const fetchHolidays = async (countryCode) => {
+  const response = await fetch(
+    `https://calendarific.com/api/v2/holidays?&api_key=${apiKey}&country=${countryCode}&year=2020`
+  );
+  return response.json();
+};
 
-  function getData(event) {
-    getCountry(event.target.value);
-    setSearch(false);
-    console.log(event.target.value);
+export default function Gettinginfo() {
+  const [inputValue, setInputValue] = useState(null);
+  const [countryCode, setCountryCode] = useState("US");
+
+  const { status, data, error, isLoading } = useQuery(
+    ["holidays", countryCode],
+    () => fetchHolidays(countryCode),
+    {
+      staleTime: Infinity,
+    }
+  );
+
+  function handleInputChange(event) {
+    setInputValue(event.target.value);
   }
 
-  const fetchHolidays = async () => {
-    const response = await fetch(
-      `https://calendarific.com/api/v2/holidays?&api_key=${apiKey}&country=US&year=2020`
-    );
-    return response.json();
-  };
+  function handleFindClick() {
+    setCountryCode(inputValue);
+  }
 
-  const { status, data, error } = useQuery("holidays", fetchHolidays, {
-    staleTime: Infinity,
-  });
   const holidays = data && data.response && data.response.holidays;
-  // console.log(data.response.holidays);
 
   return (
     <>
       <div>
-        {search ? <h1>{country}</h1> : null}
+        {countryCode && <h1>{countryCode}</h1>}
 
         <input
           className="homePageinput-field"
           type="text"
-          onChange={getData}
+          onChange={handleInputChange}
         ></input>
-        <button onClick={() => setSearch(true)}>Find holidays</button>
+        <button onClick={handleFindClick}>Find holidays</button>
       </div>
       <div>
-        {holidays?.map((holiday, i) => (
-          <div key={i}>{holiday.name}</div>
-        ))}
+        {isLoading ? (
+          <p>Loading...</p>
+        ) : (
+          holidays?.map((holiday, i) => <div key={i}>{holiday.name}</div>)
+        )}
       </div>
     </>
   );
